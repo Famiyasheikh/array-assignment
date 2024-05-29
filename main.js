ee33d4308b7f93b8914fb5a8967e1350a7ee7a63
Famiyasheikh <famiyasheikh1@gmail.com>
2024-05-29 20:07:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+"use strict";
+/// ARRAY  ASSIGNMENT \\\
+//QUESTION NO 1 \\
+//let fruit = ["apple" , "banana", "mango","orange"]
+//console.log(fruit);
+//QUESTION NO 2\\
+//let number = ["10", "20", "30","40"]
+//console.log(number);
+//QUESTION NO 3\\
+//let thirdfruit=["apple" , "banana", "mango","orange"]
+//console.log(thirdfruit[3]);
+//QUESTION NO 4\\
+//number[1]=25
+//QUESTION  NO 5\\
+//let fruit = ["apple" , "banana", "mango","orange"]
+//fruit.push("grapes")
+//console.log(fruit);
+// QUESTION  NO 6\\
+let lastfruit = ["apple", "banana", "mango", "orange"];
+lastfruit.pop();
+//console.log(lastfruit);
+//QUESTION  NO 7\\
+//let firstfruit =["apple" , "banana", "mango","orange"]
+//firstfruit.shift()
+//console.log(firstfruit);
+//QUESTION  NO 8\\
+///let fruit =["apple" , "banana", "mango","orange"]
+//fruit.unshift("kiwi")
+//console.log(fruit);
+//QUESTION  NO 9\\
+//let fruit = ["kiwi" ,"apple" , "banana", "mango","orange"]
+//fruit.splice(1,2)
+//console.log(fruit);
+//QUESTION  NO 10\\
+let fruit = ["kiwi", "apple", "banana", "mango", "orange"];
+fruit.splice(2, 0, "pineapple", "pear");
+console.log(fruit);
---
+++ main.ts
@@ -0,0 +1,67 @@
+      /// ARRAY  ASSIGNMENT \\\
+      //QUESTION NO 1 \\
+
+//let fruits = ["apple" , "banana", "mango","orange"]
+//console.log(fruits);
+
+      //QUESTION NO 2\\
+
+//let number = ["10", "20", "30","40"]
+//console.log(number);
+
+      //QUESTION NO 3\\
+
+//let thirdfruit=["apple" , "banana", "mango","orange"]
+//console.log(thirdfruit[3]);
+
+      //QUESTION NO 4\\
+
+//numbers [1] = 25;
+//console.log(numbers);
+     
+       //QUESTION  NO 5\\
+
+ //let fruits = ["apple" , "banana", "mango","orange"]
+// fruits.push("grapes")
+// console.log(fruits);
+
+      // QUESTION  NO 6\\
+
+//let lastfruit =["apple" , "banana", "mango","orange"]
+//lastfruit.pop()
+//console.log(lastfruits);
+
+       //QUESTION  NO 7\\
+
+//let firstfruit =["apple" , "banana", "mango","orange"]
+//firstfruit.shift()
+//console.log(firstfruit);
+
+       //QUESTION  NO 8\\
+
+//let fruits =["apple" , "banana", "mango","orange"]
+//fruits.unshift("kiwi")
+//console.log(fruits);
+
+     //QUESTION  NO 9\\
+
+//let fruits = ["kiwi" ,"apple" , "banana", "mango","orange"]
+//fruits.splice(1,2)
+//console.log(fruits);
+
+      //QUESTION  NO 10\\
+
+//let fruits =["kiwi" ,"apple","banana", "mango","orange"]
+//fruits.splice (2,0 ,"pineapple","pear")
+//console.log(fruits);
+
+       // QUESTION  NO 11\\
+
+//let criusfruits= ["apple", "banana"]
+//console.log(criusfruits);
+
+        //QUESTION NO 12\\
+
+//let lasttwofruits= ["mango","orange"]
+//console.log (lasttwofruits);
+        
